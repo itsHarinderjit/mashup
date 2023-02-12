ad0cc8d8ee220f5b2d9b49c559b8b2b6212c6773
harinderjit <harinderjit91218@gmail.com>
2023-02-13 00:48:55 +0530

    email module added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import os, sys
+import os
 from pytube import YouTube
 
 @st.cache_resource
@@ -11,14 +11,19 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from pytube import YouTube
 from moviepy.editor import *
-import sys
 import os
 import re
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from zipfile import ZipFile
+from zipfile import ZIP_BZIP2
 
 def download_files(singerName,n) :
     check = n
-    # st.write('inside download files')
     opts = FirefoxOptions()
     opts.add_argument("--headless")
     browser = webdriver.Firefox(options=opts)
@@ -33,7 +38,6 @@ def download_files(singerName,n) :
     scroll = 0
     while check > 0 :
         browser.execute_script('window.scrollTo({}, {})'.format(8080*scroll,8080*(scroll+1)))
-        st.write('scrolling')
         time.sleep(15)
         check -= 20
         scroll += 1
@@ -44,19 +48,13 @@ def download_files(singerName,n) :
         links.append(l.get_attribute("href"))
     
     i = 0
-    # st.write('middle of download files')
-    # st.write(links)
-    st.write(len(links))  ## remove later
-    # st.write('Currently downloading ...')
     for link in links :
-        st.write(i)
         if n==0 :
             break
         if link == None :
             continue
         try :
             yt = YouTube(link)
-            st.write(yt.length)  ## remove later
             if(yt.length >= 120 and yt.length <= 360) :
                 yt.streams.get_audio_only().download(filename='audio'+str(i)+'.mp3')
                 n -= 1
@@ -66,23 +64,54 @@ def download_files(singerName,n) :
             print('internal error')
 
 def audio_merge(n,y) :
-    # st.write('inside audio merge')
-    final_audio = AudioFileClip(r'audio0.mp3')
-    final_audio = final_audio.cutout(0,y)
-    data.write('Creating your mashup .... ')
-    for i in range(1,n) :
-        audiofile = AudioFileClip(r'audio' + str(i) + '.mp3')
-        audiofile = audiofile.cutout(0,y)
-        final_audio = concatenate_audioclips([final_audio,audiofile])
-
+    data.write('Creating your mashup....')
+    audio_list = []
+    for i in range(n) :
+        audio = AudioFileClip(r'audio'+str(i)+'.mp3')
+        audio = audio.subclip(0,y)
+        audio_list.append(audio)
+    final_audio = concatenate_audioclips(audio_list)
     final_audio.write_audiofile('output.mp3')
+    for lst in audio_list :
+        lst.close()
 
+    ## deleting audio files
     for i in range(n) :
         os.remove('audio'+str(i)+'.mp3')
+
+    ## creating zip file
+    with ZipFile(f'zipfile.zip','w',compression= ZIP_BZIP2 , allowZip64=True, compresslevel=9) as zip:
+        zip.write('output.mp3')
+
+    os.remove('output.mp3')
     data.empty()
 
-    # archive = shutil.make_archive('send.mp3','zip','output.mp3')
+def send_email(mailid) :
+    data.write('Sending email....')
+    fromaddr = "hsingh91218@gmail.com"
+    toaddr = mailid
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Mashup"
+    body = "Enjoy your mashup :) "
+    msg.attach(MIMEText(body, 'plain'))
+    attachment = open('zipfile.zip', "rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+  
+    encoders.encode_base64(p)
+   
+    p.add_header('Content-Disposition', "attachment; filename= %s" % "mashup.zip")
+    msg.attach(p)
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.starttls()
+    s.login(fromaddr, 'pgdyzgfabqypxctb')
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
 
+    data.empty()
     st.info('process completed')
 
 st.title('Mashup')
@@ -94,7 +123,7 @@ with st.form('input') :
 
     numSongs = st.text_input('Enter number of songs')
 
-    y = st.text_input('Enter time')
+    y = st.text_input('Enter length of each song')
 
     email = st.text_input('Enter your email')
 
@@ -104,17 +133,21 @@ data = st.empty()
 
 if submit_button :
     if singername != '' and numSongs != '' and y != '' and email != '' :
+        singername = singername.split()[0]
+        numSongs = numSongs.split()[0]
+        y = y.split()[0]
+        email = email.split()[0]
         regex = '[A-Za-z0-9_]*@[A-Za-z]*\.[A-Za-z]*'
         match = re.findall(regex,email)
         if match[0] != email :
-            # st.write(match)
             st.error('Wrong email')
         else :
-            # st.write('inside submit button')
-            download_files(singername,int(numSongs))
-            # st.write('after download_files')
-            audio_merge(int(numSongs),int(y))
-            # st.write('after audio merge')
+            try :
+                download_files(singername,int(numSongs))
+                audio_merge(int(numSongs),int(y))
+            except :
+                st.error('Wrong data in number of songs or length of each song')
+            send_email(email)
     else :
         st.error('Please enter data in all fields')
 
